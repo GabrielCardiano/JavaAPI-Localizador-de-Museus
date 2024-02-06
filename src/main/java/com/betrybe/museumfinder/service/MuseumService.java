ff752e628422629f527ea9ed4ea4e6d85699e966
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-02-06 01:49:45 -0300

    feat: req 3 - add InvalidCoordinateException and createMuseum() in MuseumService class.

---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,44 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * The type Museum service.
+ */
+@Service
+public class MuseumService implements MuseumServiceInterface {
+
+  MuseumFakeDatabase database;
+
+  @Autowired
+  public MuseumService(MuseumFakeDatabase database) {
+    this.database = database;
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    return null;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+    Coordinate coordinate = museum.getCoordinate();
+    if (!CoordinateUtil.isCoordinateValid(coordinate)) {
+      throw new InvalidCoordinateException();
+    }
+
+    Museum newMuseum = database.saveMuseum(museum);
+    return newMuseum;
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}

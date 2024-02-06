38fcce7bb2cb2ddd5e40655430a4d543cc77cc5c
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-02-06 15:38:52 -0300

    feat: req 6 - create endpoint GET /museums/closest

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,18 +1,18 @@
 package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
-import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -46,5 +46,26 @@ public class MuseumController {
 
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(service.createMuseum(museum));
+  }
+
+  /**
+   * Gets closest museum.
+   *
+   * @param lat         the lat
+   * @param lng         the lng
+   * @param max_dist_km the max dist km
+   * @return the closest museum
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<Museum> getClosestMuseum(@RequestParam String lat, String lng,
+      String max_dist_km) {
+    Double latitude = Double.parseDouble(lat);
+    Double longitude = Double.parseDouble(lng);
+    Double distanceInKm = Double.parseDouble(max_dist_km);
+
+    Coordinate coordinate = new Coordinate(latitude, longitude);
+
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(service.getClosestMuseum(coordinate, distanceInKm));
   }
 }
